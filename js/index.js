12e09f6542c1dc3e3ac34d5494b7bdb50e7d0a18
hannah_zeng <hzeng9310@gmail.com>
2024-09-08 16:33:20 -0400

    Added a page title, description paragraph, and the drink list.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+const coffeeList = document.getElementById("coffee-list");
+
+//Fetch hot coffees
 fetch("https://api.sampleapis.com/coffee/hot")
   .then((response) => {
     if (!response.ok) {
@@ -8,7 +11,56 @@ fetch("https://api.sampleapis.com/coffee/hot")
   })
   .then((data) => {
     const hotCoffees = data;
-    console.log(hotCoffees);
+    //console.log(hotCoffees);
+
+    hotCoffees.forEach((coffee) => {
+      const coffeeItem = document.createElement("div");
+      coffeeItem.classList.add("card");
+      const coffeeImage = document.createElement("img");
+      const coffeeName = document.createElement("p");
+      coffeeImage.src = coffee.image;
+      coffeeImage.alt = coffee.title;
+      coffeeName.textContent = coffee.title;
+      coffeeItem.appendChild(coffeeImage);
+      coffeeItem.appendChild(coffeeName);
+      coffeeList.appendChild(coffeeItem);
+    });
+  })
+  .catch((error) => {
+    console.error("An error occurred: ", error);
+  });
+
+//Fetch iced coffees
+const icedCoffeeToShow = ["Iced Coffee", "Frappuccino"];
+fetch("https://api.sampleapis.com/coffee/iced")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Fetch request failed!");
+    }
+
+    return response.json();
+  })
+  .then((data) => {
+    const icedCoffees = data;
+    //console.log(icedCoffees);
+
+    const filtedIcedCoffee = icedCoffees.filter((coffee) => {
+      return icedCoffeeToShow.includes(coffee.title);
+    });
+    //console.log(filtedIcedCoffee);
+
+    filtedIcedCoffee.forEach((coffee) => {
+      const coffeeItem = document.createElement("div");
+      coffeeItem.classList.add("card");
+      const coffeeImage = document.createElement("img");
+      const coffeeName = document.createElement("p");
+      coffeeImage.src = coffee.image;
+      coffeeImage.alt = coffee.title;
+      coffeeName.textContent = coffee.title;
+      coffeeItem.appendChild(coffeeImage);
+      coffeeItem.appendChild(coffeeName);
+      coffeeList.appendChild(coffeeItem);
+    });
   })
   .catch((error) => {
     console.error("An error occurred: ", error);
